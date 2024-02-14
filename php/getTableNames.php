03c30e6e3166969385a2391d6e840e993110938b
yenon118 <yenon118@gmail.com>
2024-02-14 00:33:11 -0600

    Added Improvement_Status as key_column for EU_Soy309 in getTableNames.php

--- php/getTableNames.php
+++ php/getTableNames.php
@@ -20,7 +20,7 @@ function getTableNames($dataset){
 		$gff_table = "act_Soybean_Wm82a2v1_GFF";
 		$accession_mapping_table = "act_Soy111_Accession_Mapping";
 	} elseif ($dataset == "EU_Soy309") {
-		$key_column = "";
+		$key_column = "Improvement_Status";
 		$gff_table = "act_Soybean_Wm82a2v1_GFF";
 		$accession_mapping_table = "act_EU_Soy309_Accession_Mapping";
 	} else {
